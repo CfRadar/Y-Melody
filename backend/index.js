fc048474a3fea34d4226531840d3dc33901620b2
CfRadar <anurag312006yadav@gmail.com>
2026-05-15 08:45:55 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://y-melody.vercel.app", "http://localhost:5173", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use("/youtube", youtubeRoute);
 app.use("/user", userRoute);
 
